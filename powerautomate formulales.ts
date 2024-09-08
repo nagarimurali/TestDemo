aa27fb1d9398d21f5abdf54af6105cd8919be97f
N Murali <113176341+nagarimurali@users.noreply.github.com>
2024-09-09 00:38:30 +0530

    Update powerautomate formulales.ts

--- powerautomate formulales.ts
+++ powerautomate formulales.ts
@@ -2,3 +2,5 @@
   "Accept": "application/json;odata=verbose"
 }
 _api/web/lists/getbytitle('TaskStatus')/items(@{outputs('Update_item')?['body/ID']})/roleassignments/addroleassignment(principalid=@{items('Apply_to_each')?['Id']},roledefid=1073741826)
+
+/_api/web/lists(guid'@{outputs('Compose_2')}')/items(@{outputs('Update_item')?['body/ID']})/roleassignments?$expand=RoleDefinitionBindings
